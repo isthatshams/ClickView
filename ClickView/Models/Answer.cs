20031c44369fc19b95269823e1156f87c6ea43bb
Mohammad Shams <mohammedmahdishams@gmail.com>
2025-04-21 11:16:39 +0300

    adding unsaved classes

--- ClickView/Models/Answer.cs
+++ ClickView/Models/Answer.cs
@@ -5,7 +5,7 @@
         public int AnswerId { get; set; }
         public int UserId { get; set; }
         public int QuestionId { get; set; }
-        public string UserAnswerText { get; set; }
+        public string UserAnswerText { get; set; } // Stores the User's answer text
         public string CorrectAnswerText { get; set; } // Stores the correct answer text
 
     }
